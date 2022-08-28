73fe31d5dbd5a50ddfc82549f04ea4fbbc3f0965
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-08-28 10:05:16 -0300

    billetes dp

--- p1/billetes.cpp
+++ p1/billetes.cpp
@@ -16,26 +16,31 @@ pair<int, int> mejor(pair<int, int> a, pair<int, int> b) {
     return b;
 }
 
-pair<int, int> cc(int i, int c) {
+pair<int, int> DP(int i, int c, matrix<pair<int, int>>& M) {
     if (c <= 0) {
         return {0, 0};
-    } else if (i == -1) {
+    }
+    else if (i == -1) {
         return {1e9, 1e9};
     }
     else {
-        pair<int, int> b = {B[i], 1};
-        return mejor(b + cc(i-1, c - B[i]), cc(i-1, c));
+        pair<int, int> s = {B[i], 1};
+        M[i][c-1] = mejor(s + DP(i-1, c - B[i], M), DP(i-1, c, M));
     }
+    return M[i][c-1];
 }
 
 int main () {
     cin >> c;
     cin >> n;
     B.resize(n);
+    matrix<pair<int, int>> M(n);
     for (int i = 0; i < n; ++i) {
         cin >> B[i];
+        M[i] = vector<pair<int, int>>(c, {-1, -1});
+        M[i][0] = {0, 0};
     }
-    pair<int, int> res = cc(n-1, c);
+    pair<int, int> res = DP(n-1, c, M);
     cout << res.first << ' ' << res.second;
     return 0;
 }
