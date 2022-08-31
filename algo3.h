a6d353c325a222777d1a4e85cf8e26e7a9743247
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-08-31 10:33:08 -0300

    cout << m

--- algo3.h
+++ algo3.h
@@ -4,6 +4,17 @@ template <typename T>
 using matrix = std::vector<std::vector<T>>;
 
 template <typename T>
+std::ostream& operator<<(std::ostream& os, const matrix<T>& m) {
+    for (int i = 0; i < m.size(); ++i) {
+        for (int j = 0; j < m[i].size(); ++j) {
+            std::cout << m[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    return os;
+}
+
+template <typename T>
 std::istream& operator>>(std::istream& is, matrix<T>& m) {
     for (int i = 0; i < m.size(); ++i) {
         for (int j = 0; j < m.size(); ++j) {
