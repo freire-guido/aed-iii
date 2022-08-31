29a17935ab4e142d49f5548891ef7215ce88b81c
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-08-30 21:02:38 -0300

    cajas BU :)

--- p1/cajas.cpp
+++ p1/cajas.cpp
@@ -6,10 +6,21 @@ vector<int> w;
 vector<int> s;
 
 int BU(int Sm) {
-    int Sm = 0;
-    for (int i = n; i >= 0; --i) {
-        for (int j = 0; j <)
+    Sm++;
+    matrix<int> M(n);
+    M[n - 1].resize(Sm);
+    for (int c = 0; c < Sm; ++c) {
+        M[n - 1][c] = c - w[n - 1] >= 0;
     }
+    for (int i = n - 2; i >= 0; --i) {
+        M[i].resize(Sm);
+        for (int c = 0; c < Sm; ++c) {
+            int con = min(c - w[i], s[i]) < 0 ? -1e9 : 1 + M[i + 1][min(c - w[i], s[i])];
+            int sin = M[i + 1][c];
+            M[i][c] = max(con, sin);
+        }
+    }
+    return M[0][Sm - 1];
 }
 
 int BT(int i, int c) {
@@ -26,16 +37,16 @@ int main() {
     cin >> n;
     matrix<int> M(n);
     w.resize(n); s.resize(n);
-    int Sm = 0;
+    int Wm = 0;
     for (int i = 0; i < n; ++i) {
-        cin >> s[i];
-        if (s[i] > Sm) {
-            Sm = s[i];
+        cin >> w[i];
+        if (w[i] > Wm) {
+            Wm = w[i];
         }
     }
     for (int i = 0; i < n; ++i) {
-        M[i].resize(Sm);
-        cin >> w[i];
+        cin >> s[i];
+        M[i].resize(Wm);
     }
-    cout << DP(0, INT_MAX);
+    cout << BU(Wm);
 }
