d811ff00dfaba46ba089477157e0f4db946a2d15
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-09-11 18:58:59 -0300

    aspersores test

--- tp1/aspersores.cpp
+++ tp1/aspersores.cpp
@@ -6,14 +6,14 @@ int n;
 int l;
 int w;
 
-bool cubrio(int i, int k) {
+bool cubrio(int i, float k) {
     return (pow(asper[i].second, 2) - pow(asper[i].first - k, 2)) >= pow(w, 2) / 4;
 }
 
 int aspersores() {
     int j = 0;
     int acum = 0;
-    for (float k = 0; k < l; k += asper[j].first + sqrt(pow(asper[j].second, 2) - pow(w, 2) / 4)) {
+    for (float k = 0; k < l; k = asper[j].first + sqrt(pow(asper[j].second, 2) - pow(w, 2) / 4)) {
         int i = j;
         for (; i < n; ++i) {
             if (cubrio(i, k) && asper[i].first + asper[i].second >= asper[j].first + asper[j].second) {
@@ -29,11 +29,12 @@ int aspersores() {
 }
 
 int main() {
-    cin >> n >> l >> w;
-    asper.resize(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> asper[i].first >> asper[i].second;
+    while (cin >> n >> l >> w) {
+        asper.resize(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> asper[i].first >> asper[i].second;
+        }
+        sort(asper.begin(), asper.end());
+        cout << aspersores() << endl;
     }
-    sort(asper.begin(), asper.end());
-    cout << aspersores();
 }
