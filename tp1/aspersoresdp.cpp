bd8c4494ee7a161bc25aab3e82b199a711975099
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-09-17 10:18:12 -0300

    aspersor k en 0

--- tp1/aspersoresdp.cpp
+++ tp1/aspersoresdp.cpp
@@ -4,50 +4,44 @@ using namespace std;
 int n, l, w;
 vector<vector<int>> M;
 
-double a(int i) { return M[i][0] - sqrt(pow(M[i][1], 2) - pow(M[0][1], 2) / 4); }
-
-double aComp(int p, int r) { return p - sqrt(pow(r, 2) - pow(r, 2) / 4); }
-
-double b(int i) { return M[i][0] + sqrt(pow(M[i][1], 2) - pow(M[0][1], 2) / 4); }
-
-bool comp(const vector<int>& a, const vector<int>& b) {
-    return aComp(a[0],a[1]) < aComp(b[0],b[1]);
+float a(int i) {
+    return M[i][0] - sqrt(pow(M[i][1], 2) - pow(w, 2) / 4);
 }
 
-bool dejansinregar(int i, int k) {
-    if (k == -1) {
-        return a(i) > 0.00001;
-    } else if (i==n-1){
-        return a(i) > b(k) or b(i) < l;
-    } else {
-        return a(i) > b(k);
-    }
+float b(int i) {
+    return M[i][0] + sqrt(pow(M[i][1], 2) - pow(w, 2) / 4);
+}
+
+bool comp(const vector<int>& a, const vector<int>& b) {
+    return a[0] - sqrt(pow(a[1], 2) - pow(w, 2) / 4) < b[0] - sqrt(pow(b[1], 2) - pow(w, 2) / 4);
+}
+
+bool dejasinregar(int i, int k) {
+    if (i == n + 1) return b(k) < l;
+    return a(i) > b(k);
 }
 
 int BT(int i, int k) {
-    if (i == M.size() && b(k) >= l) {
+    if (b(k) >= l) {
         return 0;
-    } else if(i == M.size() && b(k) < l) {
+    } else if (dejasinregar(i, k)) {
         return 1e9;
     } else {
-        if (dejansinregar(i, k)) {
-            return 1e9;
-        } else {
-            return min(BT(i + 1, k), M[i][2] + BT(i + 1, i));
-        }
+        return min(BT(i + 1, k), M[i][2] + BT(i + 1, i));
     }
 }
 
 int main() {
     cin >> n >> l >> w;
-    M.resize(n);
-    for (int i = 0; i < n; ++i) {
+    M.resize(n + 1);
+    M[0] = {0, w, 0};
+    for (int i = 1; i < n + 1; ++i) {
         M[i].resize(3);
         cin >> M[i][0] >> M[i][1] >> M[i][2];
     }
-    sort(M.begin(), M.end(), &comp);
-    for (int i = 0; i < n; ++i) {
+    sort(M.begin() + 1, M.end(), &comp);
+    for (int i = 0; i < n + 1; ++i) {
         cout << M[i][0] << " " << M[i][1] << " " << M[i][2] << endl;
     }
-    cout << BT(0, -1);
+    cout << BT(1, 0);
 }
