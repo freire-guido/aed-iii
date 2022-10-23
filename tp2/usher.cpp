d89ef6242a74a54916887d83d9e7a6f88fd9827a
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-10-23 13:50:19 -0300

    usher borrador

--- tp2/usher.cpp
+++ tp2/usher.cpp
@@ -3,33 +3,51 @@
 using namespace std;
 
 int c, p, q;
-vector<int> ady[501];
-int dist[501][501];
+vector<pair<int, int>> ady[501];
+vector<int> vuelven;
+int dist[501];
 
 int dijkstra() {
-
+    priority_queue<int, vector<int>, greater<int>> cp;
+    cp.push(0);
+    while (!cp.empty()) {
+        int u = cp.top(); cp.pop();
+        for (auto v: ady[u]) {
+            if (dist[v.first] > dist[u] + v.second) {
+                dist[v.first] = dist[u] + v.second;
+                cp.push(v.first);
+            }
+        }
+    }
+    int res = INF;
+    for (int f: vuelven) {
+        res = min(res, dist[f]);
+    }
+    return res;
 }
 
 int main() {
     int t; cin >> t;
     while (cin >> c >> p) {
-        for (int i = 0; i <= p; ++i) ady[i].clear();
-        for (int i = 0; i <= p; ++i) for (int j = 0; j <= p; ++j) dist[i][j] = INF;
+        for (int i = 0; i <= p; ++i) {
+            ady[i].clear();
+            dist[i] = INF;
+        }
         cin >> q;
         for (int i = 0; i < q; ++i) {
             int f; cin >> f;
-            ady[0].push_back(f);
-            dist[0][f] = 0;
+            ady[0].push_back({f, 0});
         }
-        for (int u = 0; u < p; ++u) {
+        for (int u = 1; u <= p; ++u) {
             int r; cin >> r;
             for (int i = 0; i < r; ++i) {
             //  (!) Puede ocurrir que dos reglas distintas
             //      definan el pasaje de la caja hacia un mismo feligres.
                 int m, v; cin >> m >> v;
-                ady[u].push_back(v);
-                dist[u][v] = m;
+                if (v == 0) vuelven.push_back(u);
+                ady[u].push_back({v, m});
             }
         }
+        cout << c / dijkstra() - 1 << endl;
     }
 }
