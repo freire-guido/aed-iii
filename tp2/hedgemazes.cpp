10cbbef482fda50fd7f79561cb13cb11d273d2fd
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-10-24 10:36:27 -0300

    dfs con parent

--- tp2/hedgemazes.cpp
+++ tp2/hedgemazes.cpp
@@ -4,23 +4,45 @@ using namespace std;
 
 int r, c, q;
 vector<int> ady[10000];
-int res[10000][10000];
+int parent[10000];
 
-int dfs() {
-    
+void dfs() {
+    for (int u = 0; u < r; ++u) {
+        if (parent[u] == -1) {
+            parent[u] = u;
+            dfs_visit(u);
+        }
+    }
+}
+
+void dfs_visit(int u) {
+    for (int v: ady[u]) {
+        if (parent[v] == -1) {
+            parent[v] = u;
+            dfs_visit(v);
+        } else {
+            parent[v] = v;
+        }
+    }
 }
 
 int main() {
     while (cin >> r >> c >> q) {
-        for (int i = 0; i < r; ++i) ady[i].clear();
-        for (int i = 0; i < r; ++i) for (int j = 0; j < r; ++j) res[i][j] = 0;
+        for (int i = 0; i < r; ++i) {
+            ady[i].clear();
+            parent[i] = -1;
+        }
         for (int i = 0; i < c; ++i) {
             int a, b; cin >> a >> b;
-            ady[a - 1].push_back(b - 1);           
+            ady[a - 1].push_back(b - 1);
+            ady[b - 1].push_back(a - 1);
         }
+        dfs();
+
         for (int i = 0; i < q; ++i) {
             int s, t; cin >> s >> t;
             cout << (res[s - 1][t - 1] == 1 ? "Y" : "N") << endl;
         }
+        cout << "-" << endl;
     }
 }
