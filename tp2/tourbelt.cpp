2c1681cb9abab5a8c151946d3d15451bca732afa
freire-guido <14301656+freire-guido@users.noreply.github.com>
2022-10-26 13:41:50 -0300

    krusky basado

--- tp2/tourbelt.cpp
+++ tp2/tourbelt.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 struct Arista {
     int u, v, p;
-    Arista(int a, int b, int c) { u = a; v = b; p = v; }
-    bool operator>(const Arista& a) { return p > a.p; }
+    Arista(int a, int b, int c) { u = a; v = b; p = c; }
+    bool operator<(const Arista& a) const { return p > a.p; }
 };
 
 int n, m;
@@ -14,24 +14,30 @@ int tam[5000];
 vector<Arista> E;
 
 int find_set(int x) {
-    return x != parent[x] ? parent[x] = find_set(parent[x]) : parent[x];
+    return parent[x] == x ? x : parent[x] = find_set(parent[x]);
 }
 
 int kruskal() {
     sort(E.begin(), E.end());
     int res = 0;
-    for (int i = 0; i < m - 1; ++i) {
-        if (find_set(E[i].u) == find_set(E[i].v)) {
-            
+    parent[find_set(E[0].u)] = find_set(E[0].v);
+    tam[find_set(E[0].v)] += tam[find_set(E[0].u)];
+    res += tam[E[0].v];
+    for (int i = 1; i < m; ++i) {
+        parent[find_set(E[i].u)] = E[i].v;
+        if (E[i].p != E[i-1].p) {
+            tam[find_set(E[i].v)] += tam[find_set(E[i].u)];
+            res += tam[E[0].v];
         }
     }
+    return res;
 }
 
 int main() {
     int t; cin >> t;
     while (cin >> n >> m) {
         E.clear();
-        for (int i = 0; i < n; ++i) parent[i] = i
+        for (int i = 0; i <= n; ++i) { parent[i] = i; tam[i] = 1; }
         for (int i = 0; i < m; ++i) {
             int v, w, k; cin >> v >> w >> k;
             E.push_back(Arista(v, w, k));
