db6b125bf37b1fb27b0b6e013cc601bc5b7170a9
Guido <14301656+freire-guido@users.noreply.github.com>
2022-10-28 15:48:10 -0300

    ady1 con puentes

--- tp2/hedgemazes.cpp
+++ tp2/hedgemazes.cpp
@@ -4,43 +4,40 @@ using namespace std;
 
 int r, c, q;
 vector<int> ady[10000];
+vector<int> ady1[10000];
 int parent[10000];
-int back[10000];
+int tin[10000];
+int puente[10000];
 
-void bfs() {
-    queue<int> c;
-    for (int u = 0; u < r; ++u) {
-        if (parent[u] == u) {
-            c.push(u);
-        }
+int dp(int s) {
+    dfs(ady);
+    int cant = 0;
+    for (int u: ady[s]) {
+        if (parent[u] == s) cant += dp(u);
+        else if (tin[s] > tin[u] && parent[s] != u) cant++;
+        else if (tin[s] < tin[u]) cant--;
     }
-    while (!c.empty()) {
-        int u = c.front(); c.pop();
-        for (int v: ady[u]) {
-            if (parent[v] != v) {
-                parent[v] = u;
-                c.push(v);
-            }
-        }
+    if (cant == 0 && parent[s] != s) {
+        ady1[s].push_back(parent[s]);
     }
+    return cant;
 }
 
-void dfs_visit(int u) {
+void dfs_visit(int u, vector<int> ady[], int t = 0) {
+    tin[u] = t++;
     for (int v: ady[u]) {
         if (parent[v] == -1) {
             parent[v] = u;
-            dfs_visit(v);
-        } else if (parent[u] != v) {
-            back[u] = v;
+            dfs_visit(v, ady, t);
         }
     }
 }
 
-void dfs() {
+void dfs(vector<int> ady[]) {
     for (int u = 0; u < r; ++u) {
         if (parent[u] == -1) {
             parent[u] = u;
-            dfs_visit(u);
+            dfs_visit(u, ady, 0);
         }
     }
 }
@@ -56,8 +53,7 @@ int main() {
             ady[a - 1].push_back(b - 1);
             ady[b - 1].push_back(a - 1);
         }
-        dfs();
-        bfs();
+        dp(0);
         for (int i = 0; i < q; ++i) {
             int s, t; cin >> s >> t;
             cout << (parent[s] == parent[t] ? "Y" : "N") << endl;
